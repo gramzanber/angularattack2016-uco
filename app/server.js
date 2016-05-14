5e2a0fdfc0c517abc1a139ce63203ed251b0221c
Drew Demechko <drew_demechko@yahoo.com>
2016-05-14 14:00:30 -0500

    Added basic REST API

--- app/server.js
+++ app/server.js
@@ -1,18 +1,53 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-   host: 'uco123.cubwz8gcpznu.us-west-2.rds.amazonaws.com',
-   user: 'admin',
-   password: 'admin123',
-   database: 'uco123' 
-});
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./REST.js");
+var app  = express();
 
-connection.connect();
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
 
-connection.query('Select * From User', function(err,rows,field) {
-   if(!err)
-    console.log('The solution is: ', rows);
-   else
-    console.log('Error while performing Query.');  
-});
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host: 'uco123.cubwz8gcpznu.us-west-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'admin123',
+        database: 'uco123', 
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
 
-connection.end();
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(3306,function(){
+          console.log("All right ! I am alive at Port 33306.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
