e2066009b843c0070bbe0691838f74f639f5dcf0
Tyrel Tachibana <ttachibana@uco.edu>
2016-05-14 12:07:06 -0500

    Added more meat to the function, making them less of a prototype

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,40 +1,15 @@
 (function () {
     'use strict';
     angular
-        .module('app')
-        .controller('playerController', function playerController($scope, $http, $q, $state, $window, $interval) {
-			$scope.songs = [];
-			$scope.songs[0] = {
-				artist: 'Omid',
-				title: 'Hacking Away',
-				album: 'Why am I still awake?',
-				year: '2016',
-				url: 'www.codeeveryday.org'
-			}
-			$scope.play = function(song){
-				$scope.currentSong = song;
-			}
-		})
-    .controller('navbar', function($scope, $http, $q, $state, $window, $interval) {
-    $scope.logged_in = false,
-    $scope.is_admin = false;
+    .module('app')
+    .controller('navbar', function($scope, $http, $q, $state, $window, $interval, $rootScope) {
+    $rootScope.logged_in = false,
+    $rootScope.is_admin = false;
 
     $scope.logout = function(user){
-      $scope.logged_in = false,
-      $scope.is_admin = false;
-    }
-    })
-    .controller('login_Controller', function($scope, $http, $q, $state, $window, $interval) {
-    $scope.register = function(){
-      $state.go('register');
-    }
-    $scope.authenticate = function(user,password){
-      $scope.logged_in = true,
-      $scope.is_admin = false;
-    }
-    })
-    .controller('upload_Controller', function($scope, $http, $q, $state, $window, $interval) {
-    $scope.add_song = function(){
+      $rootScope.logged_in = false,
+      $rootScope.is_admin = false;
+      $state.go('login');
     }
     })
     .controller('administration_Controller', function($scope, $http, $q, $state, $window, $interval) {
@@ -45,8 +20,27 @@
     $scope.delete_user = function(user){
     }
     })
-    .controller('settings_Controller', function($scope, $http, $q, $state, $window, $interval) {
-    $scope.update_user = function(user){
+    .controller('login_Controller', function($scope, $http, $q, $state, $window, $interval, $rootScope) {
+    $scope.register = function(){
+      $state.go('register');
+    }
+    $scope.authenticate = function(user,password){
+      $rootScope.logged_in = true,
+      $rootScope.is_admin = false;
+      $state.go('player');
+    }
+    })
+    .controller('playerController', function playerController($scope, $http, $q, $state, $window, $interval) {
+    $scope.songs = [];
+    $scope.songs[0] = {
+      artist: 'Omid',
+      title: 'Hacking Away',
+      album: 'Why am I still awake?',
+      year: '2016',
+      url: 'www.codeeveryday.org'
+    }
+    $scope.play = function(song){
+      $scope.currentSong = song;
     }
     })
     .controller('register_Controller', function($scope, $http, $q, $state, $window, $interval) {
@@ -54,6 +48,14 @@
       $state.go('login');
     }
     $scope.new_user = function(){
+    }
+    })
+    .controller('settings_Controller', function($scope, $http, $q, $state, $window, $interval) {
+    $scope.update_user = function(user){
+    }
+    })
+    .controller('upload_Controller', function($scope, $http, $q, $state, $window, $interval) {
+    $scope.add_song = function(){
     }
     });
 })();
