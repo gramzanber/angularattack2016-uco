24b9257bcc7a3dcf7f081bef69589c82c4b031e9
Tyrel Tachibana <ttachibana@uco.edu>
2016-05-14 16:24:42 -0500

    Just updating

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -3,6 +3,8 @@
     angular
     .module('app')
     .controller('navbar', function($scope, $http, $q, $state, $window, $interval, $rootScope) {
+    $rootScope.logged_in = $window.sessionStorage.logged_in;
+    $rootScope.is_admin = $window.sessionStorage.is_admin;
 
     $scope.logout = function(user){
       $window.sessionStorage.logged_in = false,
