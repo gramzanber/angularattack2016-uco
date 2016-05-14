03f796c2246befd128418488fbdec868016bc7a3
Omid <onowrouzi@uco.edu>
2016-05-13 22:16:51 -0500

    Changed to use ui-view instead of my-app.

---
+++ app.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules 
+        'ui.bootstrap',
+        'ui.router',
+        'ngAnimate'
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+})();
